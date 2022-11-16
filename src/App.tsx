aaf59a20581614f0d7b1f4f527e8745cb7da76c1
pablodamascenoo <pablofacex@gmail.com>
2022-11-16 11:05:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
