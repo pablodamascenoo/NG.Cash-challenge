7552547bab53c04e52c021bfe0afd2fbd04d7cb4
pablodamascenoo <pablofacex@gmail.com>
2022-11-17 14:10:43 -0300

    feat: added userContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "./contexts/UserContext.js";
+import LoginPage from "./pages/LoginPage/index.js";
+import RegisterPage from "./pages/RegisterPage/index.js";
 
 function App() {
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("UserInfo") || "{}")
+  );
+  const value = { userInfo, setUserInfo };
+
   return (
     <>
-      <BrowserRouter>
-        <Routes></Routes>
-      </BrowserRouter>
+      <UserContext.Provider value={value}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/sign-up" element={<RegisterPage />} />
+            <Route path="/" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </>
   );
 }
---
+++ src/contexts/UserContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const UserContext: any = createContext(null);
+
+export default UserContext;
